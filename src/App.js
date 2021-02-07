6e8a622b6a7e12a5fcdc259fa7621bd3313756d2
Billal ZERNENOU <billalzernenou@gmail.com>
2021-02-07 20:07:25 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Tasks from "./Tasks";
 import Header from "./Header";
+import Footer from "./Footer";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faListUl, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 library.add(faListUl, faTrashAlt);
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <Tasks />
+      <Footer />
     </div>
   );
 }
